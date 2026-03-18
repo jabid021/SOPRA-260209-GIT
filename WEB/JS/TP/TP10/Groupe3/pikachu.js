05340ada8cccafd3c8d3bdb725dd0d8c75c24cae
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-03-18 16:50:51 +0100

    Groupe3 - TP10 - V1

--- WEB/JS/TP/TP10/Groupe3/pikachu.js
+++ WEB/JS/TP/TP10/Groupe3/pikachu.js
@@ -10,28 +10,106 @@ var mouvement=30;
 var pokemon="pikachu";
 var direction="Down";
 imgPikachu.setAttribute("src","assets/img/"+pokemon+direction+".png");
+imgPikachu.setAttribute("height","100px");
+imgPikachu.setAttribute("width","100px");
+grass.setAttribute("height","700px");
+grass.setAttribute("width","700px");
 
+var leftMin = 0;
+//var rightMax = document.getElementById("grass").clientWidth - document.getElementById("imgPikachu").clientWidth;
+var rightMax = 600;
+//console.log(document.getElementById("grass").getAttribute("width"));
+//console.log(document.getElementById("imgPikachu").getAttribute("width"));
+//console.log(rightMax);
 
+var topMin = 0;
+//var bottomMax = document.getElementById("grass").clientHeight - document.getElementById("imgPikachu").clientHeight;
+var bottomMax = 600;
+console.log(bottomMax);
+
+document.getElementById("inputName").onkeyup=checkBtnValidate;
+
+function checkBtnValidate(event)
+  {
+    if(document.getElementById("inputName").value=="")
+    {
+        btnStart.disabled=true;
+    }
+    else
+    {
+        btnStart.disabled=false;
+        if(event.key=="Enter")
+        {
+          demarrerJeu();
+        }
+    }
+  }
+
+function demarrerJeu()
+{
+  let name = document.getElementById("inputName").value;
+  imgPikachu.setAttribute("title",name);
+  grass.style.display="inline";
+  formStart.style.display="none";
+  document.body.onkeydown=deplacement;
+}
+
+btnStart.onclick=demarrerJeu;
 
 function deplacement(event)
 {
   if(event.key=="ArrowDown" || event.key=="s" )
   {
-    
+    if (posY + mouvement <= bottomMax)
+    {
+      posY += mouvement;
+    }
+    else
+    {
+      posY -= mouvement;
+      setTimeout(function(){posY += mouvement;}, 700);
+    }
+    direction = "Down";
   }
   else if(event.key=="ArrowRight" || event.key=="d" )
   {
-   
+    if (posX + mouvement <= rightMax)
+    {
+      posX += mouvement;
+    }
+    else
+    {
+      posX -= mouvement;
+      setTimeout(function(){posX += mouvement;}, 700);
+    }
+    direction = "Right";
   }
 
   else if(event.key=="ArrowLeft" || event.key=="q")
   {
-    
+    if(posX - mouvement >= leftMin)
+      {
+        posX -= mouvement;
+      }
+      else
+      {
+        posX += mouvement;
+        setTimeout(function(){posX -= mouvement;}, 700);
+      }
+    direction = "Left";
   }
 
   else if(event.key=="ArrowUp" || event.key=="z")
   {
-   
+    if (posY - mouvement >= topMin){
+      posY -= mouvement;
+    }
+    else
+    {
+      posY += mouvement;
+      setTimeout(function(){posY -= mouvement;}, 700);
+    }
+    direction = "Up";
   }
   pikachu.style.top=posY+"px";
   pikachu.style.left=posX+"px";
