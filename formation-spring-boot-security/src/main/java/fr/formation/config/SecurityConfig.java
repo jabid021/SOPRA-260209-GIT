f6d19dd6857d23ff5414562570b2f4a88587dcab
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-01 13:21:15 +0000

    JOUR 1 - SECURITY USER IN MEMORY

---
+++ formation-spring-boot-security/src/main/java/fr/formation/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package fr.formation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    UserDetailsService inMemory() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        UserDetails ud = User.builder()
+            .username("jeremy")
+            // .password("{noop}123456")
+            // .password("123456")
+            .password("$2a$10$wZppzN.nawtOTtOFaunubeAYHNCWvOrwsNfKStb8n/5L3bTwpxWUW")
+            .roles("ADMIN", "USER")
+            .build()
+        ;
+
+        manager.createUser(ud);
+
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        // return NoOpPasswordEncoder.getInstance(); // PAS OUF
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        System.out.println(passwordEncoder.encode("123456"));
+
+        return passwordEncoder;
+    }
+
+}
