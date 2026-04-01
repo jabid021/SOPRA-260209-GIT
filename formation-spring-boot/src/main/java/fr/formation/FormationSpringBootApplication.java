d366c4d97e7b7639494b5fbb6339d6f14eee34dd
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-01 10:24:01 +0000

    JOUR 1 - INTRO SPRING BOOT WEB & JPA

---
+++ formation-spring-boot/src/main/java/fr/formation/FormationSpringBootApplication.java
@@ -0,0 +1,11 @@
+package fr.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationSpringBootApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FormationSpringBootApplication.class, args);
+	}
+}
