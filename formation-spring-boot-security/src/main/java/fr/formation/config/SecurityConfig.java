d40757fa4bd722b3143d05c13d9c9994e2a21c78
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-02 08:32:06 +0000

    JOUR 2 - SECURITY CONFIG - FORM & HTTP BASIC

--- formation-spring-boot-security/src/main/java/fr/formation/config/SecurityConfig.java
+++ formation-spring-boot-security/src/main/java/fr/formation/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package fr.formation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,6 +42,8 @@ public class SecurityConfig {
 
         // Configuration de la form de connexion
         http.formLogin(Customizer.withDefaults());
+
+        http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
