2384d317c7602f4e4dea2982ff55cb3b4f1f543e
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-02 13:36:16 +0000

    JOUR 2 - AUTH JWT

--- formation-spring-boot-security/src/main/java/fr/formation/controller/UtilisateurController.java
+++ formation-spring-boot-security/src/main/java/fr/formation/controller/UtilisateurController.java
@@ -1,21 +1,31 @@
 package fr.formation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.formation.config.JwtUtils;
+import fr.formation.dto.request.AuthRequest;
+import fr.formation.dto.response.TokenResponse;
 
 @RestController
 @RequestMapping("/api")
 public class UtilisateurController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/auth")
-    public String auth() {
+    public TokenResponse auth(@RequestBody AuthRequest request) {
         // On authentifie l'utilisateur ...
+        Authentication authentication = new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
 
-        UsernamePasswordAuthenticationToken simulation = new UsernamePasswordAuthenticationToken("username", "password");
+        authentication = this.authenticationManager.authenticate(authentication);
 
-        return JwtUtils.generate(simulation);
+        return new TokenResponse(JwtUtils.generate(authentication));
     }
 }
