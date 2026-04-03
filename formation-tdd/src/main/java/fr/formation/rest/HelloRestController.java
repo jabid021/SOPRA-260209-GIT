57f89c50b36c845152cfafeadb225b03c640a681
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-03 12:57:50 +0000

    JOUR 1 - MOCK JPA

--- formation-tdd/src/main/java/fr/formation/rest/HelloRestController.java
+++ formation-tdd/src/main/java/fr/formation/rest/HelloRestController.java
@@ -1,14 +1,32 @@
 package fr.formation.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.formation.dao.IDAOHello;
+import fr.formation.model.Hello;
+
 @RestController
 @RequestMapping("/api/hello")
 public class HelloRestController {
+    @Autowired
+    private IDAOHello daoHello;
+
     @GetMapping
     public String hello() {
         return "Hello world!";
+    }
+
+    @GetMapping("/all")
+    public List<Hello> helloAll() {
+        List<Hello> result = this.daoHello.findAll();
+
+        System.out.println(result);
+
+        return result;
     }
 }
--- formation-tdd/src/test/java/fr/formation/rest/HelloRestControllerTest.java
+++ formation-tdd/src/test/java/fr/formation/rest/HelloRestControllerTest.java
@@ -1,20 +1,31 @@
 package fr.formation.rest;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import fr.formation.dao.IDAOHello;
+import fr.formation.model.Hello;
+
 // @SpringBootTest
+// @AutoConfigureMockMvc // Permet d'injecter, si on utilise SpringBootTest, un MockMvc dans le contexte
+
 @WebMvcTest
 public class HelloRestControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean // Demander à Mockito de créer une instance IDAOHello dans le contexte de Spring
+    private IDAOHello daoHello;
 
     @Test
     @WithMockUser // Simuler la connexion OK d'un utilisateur
@@ -28,6 +39,23 @@ public class HelloRestControllerTest {
         // then
         result.andExpect(MockMvcResultMatchers.status().isOk());
         result.andExpect(MockMvcResultMatchers.content().string("Hello world!"));
+    }
+
+    @Test
+    @WithMockUser
+    void shouldHelloAllStatusOk() throws Exception {
+        // given
+        // Mockito.when permet de simuler des valeurs de retour
+        Mockito.when(this.daoHello.findAll()).thenReturn(List.of(new Hello()));
+
+        // when
+        ResultActions result = this.mockMvc.perform(MockMvcRequestBuilders.get("/api/hello/all"));
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+
+        // On demande à Mockito de vérifier si daoHello.findAll() a été appelée une et une seule fois
+        Mockito.verify(this.daoHello).findAll();
     }
 
 }
