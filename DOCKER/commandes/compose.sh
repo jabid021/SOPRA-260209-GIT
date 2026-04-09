bcd9d47772ec3515bc3ce90b8b886a4798b24989
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-09 14:40:07 +0000

    JOUR 2 - DEMO COMPOSE

--- DOCKER/commandes/compose.sh
+++ DOCKER/commandes/compose.sh
@@ -3,3 +3,12 @@ docker compose up -d
 
 # Supprimer les containers, les networks, pas les volumes
 docker compose down
+
+# Arrêter tous les containers du compose
+docker compose stop
+
+# Démarrer tous les containers du compose
+docker compose start
+
+# Redémarrer tous les containers du compose
+docker compose restart
