b3c6990e1eab79fc494a8b42ecd008edf8fbc876
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-09 10:45:40 +0000

    JOUR 2 - DOCKERFILE MULTI-STAGE AVEC CACHE MAVEN

--- DOCKER/docker-boot/src/main/java/fr/formation/rest/HelloRestController.java
+++ DOCKER/docker-boot/src/main/java/fr/formation/rest/HelloRestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRestController {
     @GetMapping
     public String hello() {
-        return "Hello";
+        return "Hello 3";
     }
 }
