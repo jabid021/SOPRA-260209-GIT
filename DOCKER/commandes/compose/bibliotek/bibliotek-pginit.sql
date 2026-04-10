a5e8f6dd0711ae1a97791d5c8e5122da4bc3ca4f
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-10 14:49:55 +0000

    JOUR 3 - COMPOSE BIBLIOTEK

---
+++ DOCKER/commandes/compose/bibliotek/bibliotek-pginit.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE bibliotek;
+\c bibliotek
+
+CREATE TABLE utilisateur (
+  id varchar(255) NOT NULL,
+  password varchar(150) NOT NULL,
+  username varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO utilisateur (id, username, password) VALUES ('jeremy', 'jeremy', '$2a$10$zFeTn0rQKrsMXIT2I2NAl.70YWXs05/XyJsnSsznDjB4C.T0yv8hC');
