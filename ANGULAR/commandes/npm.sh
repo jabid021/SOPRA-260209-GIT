628e4af216a3cf4bbc645d8d61b483e1d6542b40
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-20 07:59:39 +0000

    JOUR 1 - COMMANDES NPM

---
+++ ANGULAR/commandes/npm.sh
@@ -0,0 +1,15 @@
+# Voir la version
+npm -v
+
+# Installer une nouvelle dépendance sur le projet (dans le répertoire du projet, à la racine)
+# Toutes les dépendances du projet seront dans un répertoire node_modules
+npm install nom_dependance
+npm i nom_dependance
+
+# Installer les dépendances manquantes au projet (dans le répertoire du projet)
+npm install
+npm i
+
+# Installer une "application" de manière générale sur l'ordinateur
+npm install -g nom_dependance
+npm i -g nom_dependance
