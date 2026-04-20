e987cff41aeabe7d493a79e4b452240de70cd3ef
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-20 09:51:10 +0000

    JOUR 1 - COMMANDES NG

---
+++ ANGULAR/commandes/angular.sh
@@ -0,0 +1,7 @@
+# Créer un nouveau projet Angular
+ng new nom_projet
+
+# Démarrer le serveur HTTP de développement (démarrer l'application Angular - dans le répertoire du projet, à la racine)
+# Le serveur HTTP de développement écoute sur le port 4200
+ng serve
+ng s
