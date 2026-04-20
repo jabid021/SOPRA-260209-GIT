7ded725caae5dec2aea21bfab9ff15b6074fe137
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-20 10:38:17 +0000

    JOUR 1 - BIND TWO WAYS ngModel

--- ANGULAR/formation-angular/src/app/app.ts
+++ ANGULAR/formation-angular/src/app/app.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  imports: [ FormsModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
