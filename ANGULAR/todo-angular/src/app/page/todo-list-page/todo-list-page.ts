7dfe6b8ada2fae79b5cbcea21ba563336c2b6a0a
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-21 13:07:40 +0000

    JOUR 2 - CORRECTION SERVICE TODO

--- ANGULAR/todo-angular/src/app/page/todo-list-page/todo-list-page.ts
+++ ANGULAR/todo-angular/src/app/page/todo-list-page/todo-list-page.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { TodoService } from '../../service/todo-service';
 
 @Component({
   selector: 'app-todo-list-page',
@@ -10,9 +11,13 @@ import { Title } from '@angular/platform-browser';
 export class TodoListPage implements OnInit {
   // Injection de dépendance par méthode inject
   private titleService: Title = inject(Title);
+  private todoService: TodoService = inject(TodoService);
 
   // Instructions exécutées juste à l'initialisation du composant
   ngOnInit(): void {
     this.titleService.setTitle("Liste des todos");
+
+    // Vérification
+    this.todoService.findAll();
   }
 }
--- ANGULAR/todo-angular/src/app/service/todo-service.ts
+++ ANGULAR/todo-angular/src/app/service/todo-service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
+  private todos: Array<Todo> = new Array<Todo>();
 
+  public findAll(): Todo[] {
+    return this.todos;
+  }
+
+  public add(todo: Todo) {
+    this.todos.push(todo);
+  }
 }
