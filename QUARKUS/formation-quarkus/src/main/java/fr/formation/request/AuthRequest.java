583e65934d0d0f0e226ee9c817b76e1e56b8e14a
Jérémy <jeremy.perrouault@ascadis.fr>
2026-04-30 12:52:38 +0000

    JOUR 2 - SECURITY JWT

---
+++ QUARKUS/formation-quarkus/src/main/java/fr/formation/request/AuthRequest.java
@@ -0,0 +1,5 @@
+package fr.formation.request;
+
+public record AuthRequest(String username, String password) {
+
+}
---
+++ QUARKUS/formation-quarkus/src/main/java/fr/formation/response/AuthResponse.java
@@ -0,0 +1,5 @@
+package fr.formation.response;
+
+public record AuthResponse(boolean success, String token) {
+
+}
