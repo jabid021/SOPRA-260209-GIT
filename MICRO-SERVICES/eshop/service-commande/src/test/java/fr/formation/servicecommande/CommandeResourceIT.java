5a702d79fbe3ebad7650946c516755d52098676e
Jérémy <jeremy.perrouault@ascadis.fr>
2026-05-04 13:46:22 +0000

    JOUR 3 - CORRECTIONS ESHOP MS

--- MICRO-SERVICES/eshop/service-commande/src/test/java/fr/formation/servicecommande/CommandeResourceIT.java
+++
@@ -1,8 +0,0 @@
-package fr.formation.servicecommande;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class CommandeResourceIT extends CommandeResourceTest {
-    // Execute the same tests but in packaged mode.
-}
--- MICRO-SERVICES/eshop/service-stock/src/test/java/fr/formation/servicestock/StockResourceIT.java
+++
@@ -1,8 +0,0 @@
-package fr.formation.servicestock;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class StockResourceIT extends StockResourceTest {
-    // Execute the same tests but in packaged mode.
-}
